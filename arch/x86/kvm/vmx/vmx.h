e7fb97df134bad229fa0198b6aaf255087be7900
Juergen Gross <jgross@suse.com>
2024-11-04 20:06:54 +0100

    KVM: TDX: Make pmu_intel.c ignore guest TD case (jsc#PED-6143).
    suse-commit: 2586bcd7693f61926495f297bba144f526a76ff4

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -15,6 +15,7 @@
 #include "vmx_ops.h"
 #include "../cpuid.h"
 #include "run_flags.h"
+#include "pmu_intel.h"
 
 #define MSR_TYPE_R	1
 #define MSR_TYPE_W	2
@@ -91,22 +92,6 @@ union vmx_exit_reason {
 		u32	failed_vmentry		: 1;
 	};
 	u32 full;
-};
-
-struct lbr_desc {
-	/* Basic info about guest LBR records. */
-	struct x86_pmu_lbr records;
-
-	/*
-	 * Emulate LBR feature via passthrough LBR registers when the
-	 * per-vcpu guest LBR event is scheduled on the current pcpu.
-	 *
-	 * The records may be inaccurate if the host reclaims the LBR.
-	 */
-	struct perf_event *event;
-
-	/* True if LBRs are marked as not intercepted in the MSR bitmap */
-	bool msr_passthrough;
 };
 
 /*
@@ -658,21 +643,6 @@ static __always_inline struct kvm_vmx *to_kvm_vmx(struct kvm *kvm)
 static __always_inline struct vcpu_vmx *to_vmx(struct kvm_vcpu *vcpu)
 {
 	return container_of(vcpu, struct vcpu_vmx, vcpu);
-}
-
-static inline struct lbr_desc *vcpu_to_lbr_desc(struct kvm_vcpu *vcpu)
-{
-	return &to_vmx(vcpu)->lbr_desc;
-}
-
-static inline struct x86_pmu_lbr *vcpu_to_lbr_records(struct kvm_vcpu *vcpu)
-{
-	return &vcpu_to_lbr_desc(vcpu)->records;
-}
-
-static inline bool intel_pmu_lbr_is_enabled(struct kvm_vcpu *vcpu)
-{
-	return !!vcpu_to_lbr_records(vcpu)->nr;
 }
 
 void intel_pmu_cross_mapped_check(struct kvm_pmu *pmu);
